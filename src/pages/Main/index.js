138564551df2c893a5100057cd533a96baece00d
Denilson Santos <denilson.avsantos@hotmail.com>
2020-03-18 14:50:49 -0300

    building the first steps of ui

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,12 +3,7 @@ import React from 'react';
 import { Title } from './styles';
 
 function Main() {
-  return (
-    <Title>
-      Main
-      <small>menor</small>
-    </Title>
-  );
+  return <Title>Hellow World</Title>;
 }
 
 export default Main;
